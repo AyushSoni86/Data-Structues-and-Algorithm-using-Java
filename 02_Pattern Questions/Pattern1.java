1d2895b1fc32ef8bb1dec81f87e3bb9e97e9f6f4
AyushSoni86 <ayusoni86@gmail.com>
2022-10-14 11:36:35 +0530

    code modified

--- 02_Pattern Questions/Pattern1.java
+++ 02_Pattern Questions/Pattern1.java
@@ -1,5 +1,11 @@
 public class Pattern1{
     public static void main(String[] args) {
-        System.out.println("Wel to this course");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
